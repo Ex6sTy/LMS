bc327b2401a695b32b3fae58393229d320ca2859
Artemi Bogatov <aboga99@mail.ru>
2025-07-22 15:39:25 +0300

    ✅ Fix SECRET_KEY for GitHub CI

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-dev-key")
 DEBUG = os.getenv("DEBUG", "False").lower() in ["true", "1", "yes"]
 
 ALLOWED_HOSTS = []
