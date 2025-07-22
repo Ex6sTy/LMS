f3f5ab6a345f31e61187f7644d583d7a2221f648
Artemi Bogatov <aboga99@mail.ru>
2025-07-22 15:34:14 +0300

    🐛 Fix DATABASE_URL fallback for CI

--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(
+        default='postgres://postgres:arty9926@postgres:5432/LMS',
+        conn_max_age=600,
+    )
 }
 
 
